dcbd7c7408ddd92455d01907431540ed43bf7f29
Chris West <therealchriswest@hotmail.com>
2013-05-27 07:03:34 +0100

    changed to use Position() instead of x,y

--- DalekRL.py
+++ DalekRL.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
+# system imports
+import os
 
+# libtcod
 import libtcodpy as libtcod
+
+# our imports
+from interfaces import *
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 30
 
-libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'DalekRL', False)
-
+# init window
+font = os.path.join(b'resources', b'consolas10x10_gs_tc.png')
+libtcod.console_set_custom_font(font, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, b'DalekRL')
 libtcod.sys_set_fps(LIMIT_FPS)
 
+# stuff
+class Player (Drawable):
+    pass
+
+player = Player(Position(10,10),'@',libtcod.white)
+
+# handle input
+KEYMAP = {
+    'j': player.move_up,
+    'k': player.move_down, 
+    'h': player.move_left, 
+    'l': player.move_right, 
+}
+def handle_keys():
+    key = libtcod.console_wait_for_keypress(True)
+    if key.pressed and chr(key.c) in KEYMAP:
+            KEYMAP.get(chr(key.c))()
+
+# main loop
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(0,  libtcod.white)
-    libtcod.console_put_char(0, 1, 1, '@',libtcod.BKGND_NONE)
+    player.draw()
     libtcod.console_flush()
+    
+    player.clear()
+    handle_keys()
--- interfaces.py
+++ interfaces.py
@@ -2,40 +2,73 @@
 
 import libtcodpy as libtcod
 
+class Position:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __add__(self,other):
+        if isinstance(other,tuple):
+            self.x += other[0]
+            self.y += other[1]
+        else:
+            self.x += other.x
+            self.y += other.y
+        return self
+
+    def __sub__(self,other):
+        if isinstance(other,tuple):
+            self.x -= other[0]
+            self.y -= other[1]
+        else:
+            self.x -= other.x
+            self.y -= other.y
+        return self
+
+    def __gt__(self,other):
+        """Furthest from origin is largest; if tied, larger x beats larger y so that we sort left-right, top-bottom"""
+        return (self.x*self.y>other.x*other.y) or (self.x>other.x)
+    def __ge__(self,other):
+        return (self.x*self.y>other.x*other.y) or (self.x>=other.x)
+
+    def __repr__(self):
+        return "Position(%d,%d)" % (self.x,self.y)
+
+    def __str__(self):
+        return "(%d,%d)" % (self.x,self.y)
+
 
 class Drawable:
     """Can appear on the map"""
 
-    def __init__(self,x,y,symbol,colour):
-        self.x = x
-        self.y = y
+    def __init__(self,pos,symbol,colour):
+        self.pos = pos
+        print("1: %s"%pos)
         self.symbol = symbol
         self.colour = colour
 
 
-    def move(self, dx, dy):
+    def move(self, delta):
         #move by the given amount
-        self.x += dx
-        self.y += dy
+        self.pos += delta
  
-    def move_to(self, x, y):
-        self.x = x
-        self.y = y
+    def move_to(self, pos):
+        self.pos = pos
 
     def move_up(self):
-        self.y -= 1
+        self.pos += (0,-1)
     def move_down(self):
-        self.y += 1
+        self.pos += (0,1)
     def move_left(self):
-        self.x -= 1
+        self.pos += (-1,0)
     def move_right(self):
-        self.x += 1
+        self.pos += (1,0)
 
     def draw(self):
         #set the color and then draw the character that represents this object at its position
         libtcod.console_set_default_foreground(0, self.colour)
-        libtcod.console_put_char(0, self.x, self.y, self.symbol, libtcod.BKGND_NONE)
+        libtcod.console_put_char(0, self.pos.x, self.pos.y, self.symbol, libtcod.BKGND_NONE)
  
     def clear(self):
         #erase the character that represents this object
-        libtcod.console_put_char(0, self.x, self.y, ' ', libtcod.BKGND_NONE)
+        libtcod.console_put_char(0, self.pos.x, self.pos.y, ' ', libtcod.BKGND_NONE)
